e782da6ee2da0218ca829ecf433fa3c3b53c9fc4
eladinbar <62742789+eladinbar@users.noreply.github.com>
2023-12-17 14:50:24 +0200

    Added level fade in and out, layer can no longer move when entering the level teleport

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour {
+    private static readonly int FadeOut = Animator.StringToHash("FadeOut");
+
+    private PlayerInput playerInput;
+
+    [SerializeField] Animator fadeAnimator;
     [SerializeField] float levelLoadDelay = 1.5f;
+
+    private void Start() {
+        playerInput = PlayerMechanics.Instance.GetComponent<PlayerInput>();
+    }
     
     void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player"))
@@ -12,7 +22,10 @@ public class LevelExit : MonoBehaviour {
 
     private IEnumerator LoadNextLevel() {
         GetComponent<AudioSource>().Play();
+        playerInput.DeactivateInput();
+        fadeAnimator.SetTrigger(FadeOut);
         yield return new WaitForSecondsRealtime(levelLoadDelay);
+        playerInput.ActivateInput();
         FindObjectOfType<ScenePersist>().ResetScenePersist();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
